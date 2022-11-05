266b3e3d27b15c3b0c3a585091a94d0157ef25e9
Federico Bahamonde <federicobahamonde14@gmail.com>
2022-11-05 16:52:05 -0300

    Added basic functions

--- tasks-app/src/App.jsx
+++ tasks-app/src/App.jsx
@@ -1,10 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react"
+
+import TaskCreator from "./components/TaskCreator"
 
 function App() {
+  
+  const [taskItems, setTaskItems] = useState([
+    {name: 'tarea', done: false}
+  ])
 
+  function createNewTask(taskName) {
+    if (!taskItems.find(task => task.name === taskName)) {
+      setTaskItems([...taskItems, {name: taskName, done: false}])
+    }
+  }
 
   return (
+
     <div className='App'>
+
+      <TaskCreator createNewTask={createNewTask}/>
+
+      <table>
+
+        <thead>
+          <tr>
+            <th>Tasks</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            taskItems.map(task => (
+              <tr key={task.name}>
+                <td>
+                  {task.name}
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+
+      </table>
+
     </div>
   )
 }
